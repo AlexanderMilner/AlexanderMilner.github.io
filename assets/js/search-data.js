c20bb49798030bc5df8f45e4776a68b48b04c648
AlexanderMilner <alexmilner1081@gmail.com>
2025-02-11 19:20:28 +0000

    Deploying to gh-pages from @ AlexanderMilner/AlexanderMilner.github.io@8f6592877937cc4aaa009e9e39b1d90bc3ab6d6f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-research",
           title: "Research",
-          description: "These projects are still very much under construction!!",
+          description: "These projects are still very much under construction - should be up and running shortly!",
           section: "Navigation",
           handler: () => {
             window.location.href = "/research/";
@@ -386,72 +386,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{id: "projects-computing-every-minimal-markov-basis",
-          title: 'Computing every minimal Markov basis',
-          description: "A Macaulay2 package to compute every minimal Markov basis associated with a configuration matrix",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/allMarkovBases/";
-            },},{id: "projects-rotating-matrices",
-          title: 'Rotating matrices',
-          description: "Does an invertible matrix stay invertible if you rotate its rows?",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/rotatematrices/";
-            },},{id: "projects-rotating-matrices-2",
-          title: 'Rotating matrices 2',
-          description: "Does an invertible matrix stay invertible if you rotate its rows?",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/rotatematrices2/";
-            },},{id: "projects-rotating-matrices-3",
-          title: 'Rotating matrices 3',
-          description: "Does an invertible matrix stay invertible if you rotate its rows?",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/rotatematrices3/";
-            },},{
+          section: "News",},{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
